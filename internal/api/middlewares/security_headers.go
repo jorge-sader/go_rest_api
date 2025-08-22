5ee881f43e1d085d031cf7538d1331588e64e6b0
JSader <jorge.sader@gmail.com>
2025-08-22 17:45:27 +0200

    feat: add security headers middleware for HTTP server
    Introduced a new middleware in internal/api/middlewares/security_headers.go to enhance API security.
    Added headers such as X-Frame-Options, Content-Security-Policy, and Strict-Transport-Security to protect against common web vulnerabilities.
    Included headers to control caching, cross-origin policies, and access control for better security and compatibility.

---
+++ internal/api/middlewares/security_headers.go
@@ -0,0 +1,28 @@
+package middlewares
+
+import "net/http"
+
+func SecurityHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-DNS-Prefetch-Control", "off")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("X-XSS-Protection", "1;mode=block")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("Strict-Transport-Security", "max-age=6372000;includeSubDomains;preload")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'")
+		w.Header().Set("Referrer-Policy", "no-referrer")
+		w.Header().Set("X-Powered-By", "Django") // set to wrong language as decoy
+		w.Header().Set("Server", "")
+		w.Header().Set("x-permitted-cross-domain-policies", "none")
+		w.Header().Set("cache-control", "no-store, no-cache, must-revalidate, max-age=0")
+		w.Header().Set("cross-origin-resource-policy", "same-origin")
+		w.Header().Set("cross-origin-opener-policy", "same-origin")
+		w.Header().Set("cross-origin-embedder-policy", "require-corp")
+		w.Header().Set("access-control-allow-headers", "Content-type, Authorization")
+		w.Header().Set("access-control-allow-methods", "GET, POST, PUT, PATCH, DELETE")
+		w.Header().Set("access-control-allow-credentials", "true")
+		w.Header().Set("permissions-policy", "geo-location=(self), microphone=()")
+
+		next.ServeHTTP(w, r)
+	})
+}
