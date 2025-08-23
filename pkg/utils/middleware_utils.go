bc168df3cc52243823a28b7e80be590943b7418e
JSader <jorge.sader@gmail.com>
2025-08-23 18:33:30 +0200

    feat: add utility for applying middleware chains
    Introduced ApplyMiddlewares function in pkg/utils/middleware_utils.go to simplify middleware composition.
    Defined a Middleware type for consistent handler wrapping and applied it to chain multiple middlewares dynamically.
    This utility improves maintainability and readability of middleware application.

---
+++ pkg/utils/middleware_utils.go
@@ -0,0 +1,14 @@
+package utils
+
+import "net/http"
+
+// Middleware is a function that wraps an http.Handler with additional functionality
+type Middleware func(http.Handler) http.Handler
+
+// ApplyMiddlewares applies a chain of middlewares to an http.Handler
+func ApplyMiddlewares(handler http.Handler, middlewares ...Middleware) http.Handler {
+	for _, middleware := range middlewares {
+		handler = middleware(handler)
+	}
+	return handler
+}
